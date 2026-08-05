40773534c63f0c19f9d8c7da42c9cc19ec6e3edf
Mohammad Sabty <brainstormers.acc@gmail.com>
2026-08-06 00:45:12 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,15 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
 });
